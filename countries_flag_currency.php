1119e32609ef51b7b0e2f1d86544454b219c467d
Vishal <vishal.ranjan7777@gmail.com>
2023-07-12 19:52:07 -0500

    Change title

--- countries_flag_currency.php
+++ countries_flag_currency.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Countries with Flag and currency</title>
+        <title>Countries with flag and currency</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
